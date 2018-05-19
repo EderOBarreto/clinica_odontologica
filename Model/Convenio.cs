02f22b229cd74ea18fbc7f22af9e076e657193cd
Consoli <matheusconsoli@protonmail.com>
2018-05-19 12:49:43 -0300

    Classes Modelo de Convenio, ListaConvenio, Paciente e ListaPacientes adicionadas.

---
+++ Model/Convenio.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    class Convenio
+    {
+        public int Cid { get; set; }
+        public string NomeConvenio { get; set; }
+        public string Cnpj { get; set; } // ?
+        public string Contato { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Model/ListaPacientes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace Model
+{
+    class ListaPacientes : List<Paciente>
+    {
+    }
+}
